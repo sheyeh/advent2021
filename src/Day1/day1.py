f9a9b7cf6236c6fb51910b4b8533b27d1260a035
sheyeh <sheyeh@turbonomic.com>
2022-01-24 21:00:03 +0200

    Day 1

--- src/Day1/day1.py
+++ src/Day1/day1.py
@@ -1,1 +1,16 @@
 ###
+
+def count(list, window):
+    c = 0
+    for i in range(0, len(list) - window + 2):
+        if sum(list[i + 1 : i + window + 1]) > sum(list[i : i + window]):
+            c += 1
+    return c
+
+my_list = []
+with open('../../data/day1.txt', 'r') as f:
+    for line in f:
+        my_list.append(int(line))
+
+print("Part 1 :", count(my_list, 1))
+print("Part 2 :", count(my_list, 3))
