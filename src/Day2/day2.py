5ce01b38200ef903b7fb45982698bc7d3403ee2f
Shai Benjamin <sheyeh@gmail.com>
2022-01-26 13:09:31 +0200

    Some refactoring

--- src/Day2/day2.py
+++ src/Day2/day2.py
@@ -7,31 +7,17 @@ def parse(line):
         'forward': [val, 0]
     }[direction]
 
+aim = 0 # in part 1 this is depth
 horizontal = 0
-depth = 0
+depth_part_2 = 0 # only used in part 2
 
 with open('day2.txt', 'r') as f:
     for line in f:
         x, y = parse(line)
+        aim += y
         horizontal += x
-        depth += y
+        depth_part_2 += aim * x
 
-print("Part 1:", horizontal * depth)
+print("Part 1:", horizontal * aim)
+print("Part 2:", horizontal * depth_part_2)
 
-aim = 0
-horizontal = 0
-depth = 0
-
-with open('day2.txt', 'r') as f:
-    for line in f:
-        direction, value = line.split(" ")
-        val = int(value)
-        if direction == 'up':
-            aim -= val
-        if direction == 'down':
-            aim += val
-        if direction == 'forward':
-            horizontal += val
-            depth += aim * val
-
-print("Part 2:", horizontal * depth)
