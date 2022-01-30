73ecf265a82f1b010ca19b4011f3423a8ca18a95
Shai Benjamin <sheyeh@gmail.com>
2022-01-30 02:19:44 +0200

    Day 5 part 2

--- src/Day5/day5.py
+++ src/Day5/day5.py
@@ -1,4 +1,5 @@
 import re
+import numpy
 
 
 def parse(value):
@@ -9,21 +10,33 @@ def my_range(i, j):
     return range(i, j + 1) if i < j else range(j, i + 1)
 
 
-# diagram is represented as a dictionary where the key is an x-y coordinate
-# and the value is the number of vents that go through x-y
-diagram = {}
+def increment(d, k):
+    val = d.get(k)
+    d[k] = val + 1 if val else 1
+
+
+# diagram is represented as a dictionary where the key is an (x,y) coordinate
+# and the value is the number of vents that go through (x,y)
+diagram1 = {}  # represents horizontal and vertical vents
+diagram2 = {}  # represents horizontal, vertical and diagonal vents
 with open('day5.txt', 'r') as f:
     for line in f:
         x1, y1, x2, y2 = parse(line)
-        if x1 == x2:
+        if x1 == x2:  # horizontal
             for y in my_range(y1, y2):
                 key = (x1, y)
-                val = diagram.get(key)
-                diagram[key] = val + 1 if val else 1
-        if y1 == y2:
-            for x in my_range(x1, x2):
-                key = (x, y1)
-                val = diagram.get(key)
-                diagram[key] = val + 1 if val else 1
+                increment(diagram1, key)
+                increment(diagram2, key)
+        else:  # vertical and diagonal
+            x = x1
+            y = y1
+            while x != x2 + numpy.sign(x2 - x1):
+                key = (x, y)
+                if y1 == y2:  # diagram1 is incremented only for vertical vents
+                    increment(diagram1, key)
+                increment(diagram2, key)
+                x += numpy.sign(x2 - x1)
+                y += numpy.sign(y2 - y1)
 
-print("Part 1:", sum(map(lambda v: v > 1, diagram.values())))
+print("Part 1:", sum(map(lambda v: v > 1, diagram1.values())))
+print("Part 2:", sum(map(lambda v: v > 1, diagram2.values())))
