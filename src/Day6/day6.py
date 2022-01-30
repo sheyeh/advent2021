68de65fbce63aa1b7f8b53f88e13efb4887bdea7
Shai Benjamin <sheyeh@gmail.com>
2022-01-30 17:16:17 +0200

    Day 6 part 2

--- src/Day6/day6.py
+++ src/Day6/day6.py
@@ -7,10 +7,12 @@ with open('day6.txt', 'r') as f:
 
 
 hist = list(numpy.histogram(fish, range(0, 10))[0])
-for i in range(0, 80):
+for i in range(0, 256):
     h8 = hist[0]  # fish with zero days left
     hist = hist[1:]
     hist[6] += h8  # fish with zero days reset to 6, and get added to others that have 6 days left
     hist.append(h8)  # fish with zero days create new fish with 8 days
+    if i == 79:
+        print("Part 1:", sum(hist))
 
-print("Part 1:", sum(hist))
+print("Part 2:", sum(hist))
