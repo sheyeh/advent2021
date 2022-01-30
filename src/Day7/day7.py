8e1fffb95a50a0174c3ee17865e945dbae00d0ed
Shai Benjamin <sheyeh@gmail.com>
2022-01-30 18:29:01 +0200

    Day 7 part 1 using own code

--- src/Day7/day7.py
+++ src/Day7/day7.py
@@ -13,3 +13,24 @@ def cost(center):
 # Using SciPy
 res = minimize_scalar(cost, bounds=(min(hor), max(hor)), method='bounded')
 print("Part 1:", cost(round(res.x)))
+
+# Alternative
+low = min(hor)
+high = max(hor)
+while True:
+    # Evaluate at mid point and +/- 1
+    mid = int((low + high) / 2)
+    mid_val_m1 = cost(mid - 1)
+    mid_val = cost(mid)
+    mid_val_p1 = cost(mid + 1)
+    # if mid point value is lower than both then minimum found
+    if mid_val < mid_val_m1 and mid_val < mid_val_p1:
+        break
+    # If mid value is lower than the mid -1 then move right
+    if mid_val < mid_val_m1:
+        low = mid
+    # Otherwise move left
+    else:
+        high = mid
+
+print("Part 1:", mid_val)
