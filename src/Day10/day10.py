1442fccd1f6a764742c4f310bb8fb3991da1d13b
Shai Benjamin <sheyeh@gmail.com>
2022-02-02 19:09:25 +0200

    Day 10 part 2

--- src/Day10/day10.py
+++ src/Day10/day10.py
@@ -5,23 +5,37 @@ corrupted = re.compile(r'\]|\}|\>|\)')
 
 
 def score(line0):
-    _score = 0
+    _syntax_error_score = 0
+    _autocomplete_score = 0
     while regex.search(line0):
         line0 = re.sub(regex, "", line0)
     corrupted_match = corrupted.search(line0)
     if corrupted_match:
-        _score += {
+        _syntax_error_score += {
             ')': 3,
             ']': 57,
             '}': 1197,
             '>': 25137
         }[corrupted_match.group()]
-    return _score
+    else:
+        for c in reversed(line0):
+            _autocomplete_score = _autocomplete_score * 5 + {
+                 '(': 1,
+                 '[': 2,
+                 '{': 3,
+                 '<': 4
+             }[c]
+    return _syntax_error_score, _autocomplete_score
 
 
-total_score = 0
+total_syntax_error_score = 0
+autocomplete_scores = []
 with open('day10.txt', 'r') as f:
     for line in f:
-        total_score += score(line.rstrip())
+        syntax_error_score, autocomplete_score = score(line.rstrip())
+        total_syntax_error_score += syntax_error_score
+        if autocomplete_score:
+            autocomplete_scores.append(autocomplete_score)
 
-print(total_score)
+print("Part 1:", total_syntax_error_score)
+print("Part 2:", sorted(autocomplete_scores)[int(len(autocomplete_scores) / 2)])
