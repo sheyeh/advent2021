941aff6c0be8c84274829ccc7197f6afebc3c641
Shai Benjamin <sheyeh@gmail.com>
2022-02-09 00:42:55 +0200

    Day 12 part 2 count method returns also paths

--- src/Day12/Graph.py
+++ src/Day12/Graph.py
@@ -1,9 +1,12 @@
+import copy
+
+
 class Graph:
 
     def __init__(self, graph=None):
         if graph is None:
             graph = {}
-        self._graph = graph.copy()
+        self._graph = copy.deepcopy(graph)
 
     def _add(self, x, y):
         if self._graph.get(x):
@@ -15,16 +18,38 @@ class Graph:
         self._add(x, y)
         self._add(y, x)
 
-    def count(self, source='start', visited=None):
+    def nodes(self):
+        return self._graph.keys()
+
+    def get(self, node):
+        return self._graph[node]
+
+    def count(self, source='start', visited=None, path=None, paths=[]):
         if source == 'end':
-            return 1
+            path.append(source)
+            paths.append(path)
+            return 1, paths
         if visited is None:
             visited = []
+            path = []
+        path.append(source)
         num_paths = 0
         neighbors = set(self._graph.get(source)) - set(visited)
         for nxt in neighbors:
             next_visited = visited.copy()
             if source.islower():
                 next_visited.append(source)
-            num_paths += self.count(nxt, next_visited)
-        return num_paths
+            cnt = self.count(nxt, next_visited, path.copy(), paths)[0]
+            num_paths += cnt
+        return num_paths, paths
+
+    def clone(self):
+        return Graph(copy.deepcopy(self._graph))
+
+    def duplicate(self, node):
+        dup_node = node + "%"
+        neighbors = self._graph.get(node)
+        self._graph[dup_node] = neighbors
+        for n in neighbors:
+            self._graph[n].append(dup_node)
+        return self
--- src/Day12/day12.py
+++ src/Day12/day12.py
@@ -10,4 +10,11 @@ with open('day12.txt', 'r') as f:
         toNode = ends[1]
         graph.add(fromNode, toNode)
 
-print("Part 1:", graph.count())
+print("Part 1:", graph.count()[0])
+
+paths_with_duplicates = []
+for cave in [cave for cave in set(graph.nodes()) - {'start', 'end'} if cave.islower()]:
+    paths_with_duplicates += graph.clone().duplicate(cave).count()[1]
+
+str_paths = set([str(path).replace("%", "") for path in paths_with_duplicates])
+print("Part 2:", len(str_paths))
