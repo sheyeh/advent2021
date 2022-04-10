0844d026dd8e5b36cc76b5d72b0e4ff2d6f9bbcb
Shai Benjamin <sheyeh@gmail.com>
2022-04-10 19:56:50 +0300

    Day 13 part 2

--- src/Day13/day13.py
+++ src/Day13/day13.py
@@ -13,3 +13,22 @@ with open('day13.txt', 'r') as f:
 new_dots = {dot.fold(folding_instructions[0]) for dot in input_dots}
 
 print("Part 1:", len(new_dots))
+
+dots = input_dots
+for instruction in folding_instructions:
+    new_dots = {dot.fold(instruction) for dot in input_dots}
+    dots = new_dots
+
+width = max(dot.x for dot in dots) + 1
+height = max(dot.y for dot in dots) + 1
+line = " " * width
+lines = []
+for i in range(height):
+    lines.append(list(line))
+
+for dot in dots:
+    lines[dot.y][dot.x] = "#"
+
+print("Part 2:")
+for line in lines:
+    print("".join(line))
