7b76b4eca0ba04c785ac2623a2913d7904843cd3
Shai Benjamin <sheyeh@gmail.com>
2022-04-20 02:05:08 +0300

    Day 15 part 1. Use dijkstra algorithm from https://levelup.gitconnected.com/dijkstra-algorithm-in-python-8f0e75e3f16e.

--- src/Day15/day15.py
+++ src/Day15/day15.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import heapq as heap
 
+
 # From https://levelup.gitconnected.com/dijkstra-algorithm-in-python-8f0e75e3f16e
 def dijkstra(G, startingNode):
     visited = set()
@@ -26,6 +27,7 @@ def dijkstra(G, startingNode):
 
     return parentsMap, nodeCosts
 
+
 risks = []
 with open('day15.txt', 'r') as f:
     for line in f:
@@ -36,7 +38,7 @@ N = len(risks[0])
 graph = {}
 for i in range(len(risks)):
     for j in range(len(risks[0])):
-        node = (i,j)
+        node = (i, j)
         graph[node] = {}
         if j < N - 1:
             graph[node][(i, j+1)] = risks[i][j + 1]
@@ -47,21 +49,5 @@ for i in range(len(risks)):
         if i > 0:
             graph[node][(i-1, j)] = risks[i - 1][j]
 
-distances = dijkstra(graph, (0,0))
-print("Part 1:",distances[1][(N-1,M-1)])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+distances = dijkstra(graph, (0, 0))
+print("Part 1:", distances[1][(N-1, M-1)])
