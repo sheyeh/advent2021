5b8d97d0e005b1208f753533ca669d3bf4c96107
Shai Benjamin <sheyeh@gmail.com>
2022-04-21 22:46:56 +0300

    Day 15 part 2

--- src/Day15/day15.py
+++ src/Day15/day15.py
@@ -28,26 +28,47 @@ def dijkstra(G, startingNode):
     return parentsMap, nodeCosts
 
 
+def map_to_graph(levels):
+    length = len(levels)
+    width = len(levels[0])
+    graph = {}
+    for i in range(len(levels)):
+        for j in range(len(levels[0])):
+            node = (i, j)
+            graph[node] = {}
+            if j < width - 1:
+                graph[node][(i, j+1)] = levels[i][j + 1]
+            if j > 0:
+                graph[node][(i, j-1)] = levels[i][j - 1]
+            if i < length - 1:
+                graph[node][(i+1, j)] = levels[i + 1][j]
+            if i > 0:
+                graph[node][(i-1, j)] = levels[i - 1][j]
+    return graph
+
+
 risks = []
 with open('day15.txt', 'r') as f:
     for line in f:
         risks.append([int(i) for i in line.rstrip()])
 
-M = len(risks)
-N = len(risks[0])
-graph = {}
-for i in range(len(risks)):
-    for j in range(len(risks[0])):
-        node = (i, j)
-        graph[node] = {}
-        if j < N - 1:
-            graph[node][(i, j+1)] = risks[i][j + 1]
-        if j > 0:
-            graph[node][(i, j-1)] = risks[i][j - 1]
-        if i < M - 1:
-            graph[node][(i+1, j)] = risks[i + 1][j]
-        if i > 0:
-            graph[node][(i-1, j)] = risks[i - 1][j]
+graph_1 = map_to_graph(risks)
 
-distances = dijkstra(graph, (0, 0))
-print("Part 1:", distances[1][(N-1, M-1)])
+distances = dijkstra(graph_1, (0, 0))
+M_1 = len(risks)
+N_1 = len(risks[0])
+print("Part 1:", distances[1][(N_1-1, M_1-1)])
+
+for p in range(N_1):
+    for q in range(1, 5):
+        risks[p].extend([(i + q) % 9 or 9 for i in risks[p][0:N_1]])
+
+for q in range(1, 5):
+    for p in range(M_1):
+        risks.append([(i + q) % 9 or 9 for i in risks[p]])
+
+graph_2 = map_to_graph(risks)
+distances = dijkstra(graph_2, (0, 0))
+M_2 = len(risks)
+N_2 = len(risks[0])
+print("Part 2:", distances[1][(N_2-1, M_2-1)])
