7df2b6314f2b9a78390b0f87444187459df5de71
Shai Benjamin <sheyeh@gmail.com>
2022-10-24 21:24:09 +0300

    Day 16 part 2 Change the way length is handled

--- src/Day16/day16.py
+++ src/Day16/day16.py
@@ -1,20 +1,34 @@
+import math
+
 with open('day16.txt', 'r') as f:
     for line in f:
         hex_str = line
         res = bin(int(hex_str, 16))[2:].zfill(len(hex_str)*4)
 
 
+MAX_LEN = len(res) + 1
+packet_id_dic = {
+    0: "SUM",
+    1: "PRODUCT",
+    2: "MIN",
+    3: "MAX",
+    5: "GT",
+    6: "LT",
+    7: "EQ"
+}
+
+
 def type4(bits):
-    val = 0
+    type_4_val = 0
     _pos = 0
     while True:
         stop_bit = bits[_pos]
         b = int(bits[_pos+1:_pos+5], 2)
-        val = val * 16 + b
+        type_4_val = type_4_val * 16 + b
         _pos += 5
         if stop_bit == '0':
             break
-    return val, _pos
+    return type_4_val, _pos
 
 
 pos = 0
@@ -22,47 +36,69 @@ sum_versions = 0
 count = 0
 
 
-def decode(bits):
+def decode(bits, gap=""):
     global pos
     global count
     global sum_versions
-    if int(bits[pos:]) == 0:
-        return 0, 0, len(bits) + 1
     count += 1
-    print(" " * pos + "v")
-    print(bits)
-    print("Packet #" + str(count), "at position", pos)
+    print(gap, " " * pos + "v")
+    print(gap, bits)
+    print(gap, "Packet #" + str(count), "at position", pos)
     version = int(bits[pos:pos+3], 2)
     pos = pos + 3
     sum_versions += version
     packet_id = int(bits[pos:pos+3], 2)
     pos = pos + 3
-    print("Version", version, "Type", packet_id)
+    print(gap, "Version", version, "Type", packet_id)
     if packet_id == 4:
-        val, _pos = type4(bits[pos:])
+        value, _pos = type4(bits[pos:])
         pos = pos + _pos
-        print("Value", val)
-        return version, val, _pos
+        print(gap, "Value", value)
+        return value
     else:
+        series = []
+        _len = 0
         if bits[pos] == '0':
             # length type 0 means the next 15 bits are the length in bits of the sub-packet
             length = int(bits[pos+1:pos+1+15], 2)
-            print("Length", length)
+            print(gap, "Length", length)
             pos = pos + 16
-            _len = 0
-            while _len < length:
-                ver, v, p = decode(bits)
-                _len += p
-            return version, 0, _len
+            start_pos = pos
+            while pos - start_pos < length:
+                series.append(decode(bits, gap + "  "))
         else:
             # length type 1 means the next 11 bits are the number of sub-packets
             num_sub_packets = int(bits[pos+1:pos+1+11], 2)
-            print("Number of sub-packets", num_sub_packets)
+            print(gap, "Number of sub-packets", num_sub_packets)
             pos = pos + 12
             for i in range(num_sub_packets):
-                decode(bits)
-            return version, 0, 0
+                series.append(decode(bits, gap + "  "))
+        value = 0
+        match packet_id:
+            case 0:
+                value = sum(series)
+            case 1:
+                value = math.prod(series)
+            case 2:
+                value = min(series)
+            case 3:
+                value = max(series)
+            case 5:
+                if (len(series)) != 2:
+                    raise Exception("Number of arguments is not 2", packet_id, series)
+                value = 1 if series[0] > series[1] else 0
+            case 6:
+                if (len(series)) != 2:
+                    raise Exception("Number of arguments is not 2", packet_id, series)
+                value = 1 if series[0] < series[1] else 0
+            case 7:
+                if (len(series)) != 2:
+                    raise Exception("Number of arguments is not 2", packet_id, series)
+                value = 1 if series[0] == series[1] else 0
+        print(gap, packet_id_dic.get(packet_id), series, "=", value)
+        return value
 
 
-decode(res)
+val = decode(res)
 print("Sum of versions:", sum_versions)
+print("Value:", val)
