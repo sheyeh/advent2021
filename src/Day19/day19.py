482f6697f91d0cec186d97afe268339959ab3959
Shai Benjamin <sheyeh@gmail.com>
2022-11-17 19:44:01 +0200

    fix warnings, add data file

--- src/Day19/day19.py
+++ src/Day19/day19.py
@@ -45,7 +45,7 @@ def diff(l1, l2):
     return None, None, None
 
 
-matches = {}  # matches[i][j] has the transormation from scanner j to scanner i
+matches = {}  # matches[i][j] has the transformation from scanner j to scanner i
 for i1 in range(len(beacons)):
     beacons1 = beacons[i1]
     for i2 in range(i1 + 1, len(beacons)):
@@ -59,7 +59,7 @@ for i1 in range(len(beacons)):
             matches[i2][i1] = (inv_rot, -inv_rot @ delta)
 
 
-# We can try to find shortest paths from each node to 0 but it could be simpler to just
+# We can try to find the shortest paths from each node to 0, but it could be simpler to just
 # compute the transition matrices from each point to each point in maximum |scanners| steps
 for n in range(scanner_num):
     matches2 = {}
@@ -67,34 +67,24 @@ for n in range(scanner_num):
         matches2.setdefault(i1, {})
         for i2 in range(scanner_num):
             if i1 == i2:
-                matches2[i1][i2] = (I0, [0,0,0])
+                matches2[i1][i2] = (I0, [0, 0, 0])
             elif matches.get(i1) is not None and matches[i1].get(i2) is not None:
                 matches2[i1][i2] = matches[i1][i2]
             else:
                 for k in range(scanner_num):
                     if matches.get(k) is not None and matches[k].get(i2) is not None \
-                        and matches.get(i1) is not None and matches[i1].get(k) is not None:
-                            r2 = matches[i1][k][0] @ matches[k][i2][0]
-                            d2 = matches[i1][k][0] @ matches[k][i2][1] + matches[i1][k][1]
-                            matches2[i1][i2] = (r2, d2)
-                            break
+                      and matches.get(i1) is not None and matches[i1].get(k) is not None:
+                        r2 = matches[i1][k][0] @ matches[k][i2][0]
+                        d2 = matches[i1][k][0] @ matches[k][i2][1] + matches[i1][k][1]
+                        matches2[i1][i2] = (r2, d2)
+                        break
     matches = matches2
-
-# for i1 in matches:
-#     for i2 in matches[i1]:
-#         print(">",i1,i2)
-
 
 points = {}
 for i in range(scanner_num):
-    print("Scanner", i)
     m = matches[0][i]
-    print(m)
     for b in beacons[i]:
         p = m[0] @ b + m[1]
-        points[str(p).replace(".","")] = p
-        print(b,"->",p)
+        points[str(p).replace(".", "")] = p
 
-print(points.keys())
 print(len(points))
-
