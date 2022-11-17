be67563d4ded4dbf27116c5ce5ddc47afa2a7158
Shai Benjamin <sheyeh@gmail.com>
2022-11-17 19:39:57 +0200

    Also include the deltas. Compute unique points.

--- src/Day19/day19.py
+++ src/Day19/day19.py
@@ -1,13 +1,11 @@
 import numpy as np
-from Match import Match
-
-I0 = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])   # no rotation
-Rx = np.array([[1, 0, 0], [0, 0, -1], [0, 1, 0]])  # rotation around x
-Ry = np.array([[0, 0, -1], [0, 1, 0], [1, 0, 0]])  # rotation around y
-Rz = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])  # rotation around z
 
 # Generate the 24 possible rotations
 # See https://stackoverflow.com/questions/16452383/how-to-get-all-24-rotations-of-a-3-dimensional-array
+I0 = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])   # no rotation
+Rx = np.array([[1, 0, 0], [0, 0, -1], [0, 1, 0]])  # rotation around x
+Ry = np.array([[0, 0, -1], [0, 1, 0], [1, 0, 0]])  # rotation around y - just for completeness, not used
+Rz = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])  # rotation around z
 rotations = []
 S = I0
 for r in range(6):
@@ -15,8 +13,8 @@ for r in range(6):
     rotations.append(S)
     for t in range(4):
         S = S if t == 0 else Rz @ S if r % 2 else Rz @ Rz @ Rz @ S
-        desc = "{} rolls and {} turns".format(r, t)
         rotations.append(S)
+
 
 beacons = []
 scanner_num = -1
@@ -30,32 +28,73 @@ with open('day19.txt', 'r') as f:
         else:
             beacons[scanner_num].append(np.array([int(s) for s in line.rstrip().split(",")]))
 
+scanner_num += 1
+
+
 def diff(l1, l2):
     for rotation in rotations:
         d = {}
-        d1 = {}
-        d2 = {}
         for v1 in l1:
             for v2 in l2:
                 result = v1 - rotation @ v2
                 result_key = str(result)
                 d.setdefault(result_key, 0)
-                d1.setdefault(result_key, [])
-                d2.setdefault(result_key, [])
                 d[result_key] += 1
-                d1[result_key].append(v1)
-                d2[result_key].append(v2)
                 if d[result_key] == 12:
                     return True, result, rotation
     return None, None, None
 
-matches = {}
+
+matches = {}  # matches[i][j] has the transormation from scanner j to scanner i
 for i1 in range(len(beacons)):
     beacons1 = beacons[i1]
     for i2 in range(i1 + 1, len(beacons)):
         beacons2 = beacons[i2]
         found, delta, rotation = diff(beacons1, beacons2)
         if found:
-            print(i1, i2, delta, rotation)
+            matches.setdefault(i1, {})
             matches.setdefault(i2, {})
-            matches[i2][i1] = str(rotation)
+            inv_rot = np.linalg.inv(rotation)
+            matches[i1][i2] = (rotation, delta)
+            matches[i2][i1] = (inv_rot, -inv_rot @ delta)
+
+
+# We can try to find shortest paths from each node to 0 but it could be simpler to just
+# compute the transition matrices from each point to each point in maximum |scanners| steps
+for n in range(scanner_num):
+    matches2 = {}
+    for i1 in range(scanner_num):
+        matches2.setdefault(i1, {})
+        for i2 in range(scanner_num):
+            if i1 == i2:
+                matches2[i1][i2] = (I0, [0,0,0])
+            elif matches.get(i1) is not None and matches[i1].get(i2) is not None:
+                matches2[i1][i2] = matches[i1][i2]
+            else:
+                for k in range(scanner_num):
+                    if matches.get(k) is not None and matches[k].get(i2) is not None \
+                        and matches.get(i1) is not None and matches[i1].get(k) is not None:
+                            r2 = matches[i1][k][0] @ matches[k][i2][0]
+                            d2 = matches[i1][k][0] @ matches[k][i2][1] + matches[i1][k][1]
+                            matches2[i1][i2] = (r2, d2)
+                            break
+    matches = matches2
+
+# for i1 in matches:
+#     for i2 in matches[i1]:
+#         print(">",i1,i2)
+
+
+points = {}
+for i in range(scanner_num):
+    print("Scanner", i)
+    m = matches[0][i]
+    print(m)
+    for b in beacons[i]:
+        p = m[0] @ b + m[1]
+        points[str(p).replace(".","")] = p
+        print(b,"->",p)
+
+print(points.keys())
+print(len(points))
+
